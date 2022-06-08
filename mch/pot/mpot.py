6ca5994949a6377db20311bbdf81c85a257e32cf
Yubo "Paul" Yang <pyang@flatironinstitute.org>
2022-06-07 21:00:42 -0400

    mpot

---
+++ mch/pot/mpot.py
@@ -0,0 +1,20 @@
+def mpot(rvecs, am, phi, vm):
+  bm = 4*np.pi/3**0.5/am
+  raxes = bm*np.array([
+    [3**0.5/2, 0.5],
+    [0, 1],
+  ])
+  #gfracs = np.array([
+  #  [1, 0],
+  #  [-1, 1],
+  #  [0, -1],
+  #])  # B site at (1./3, 2./3)
+  gfracs = np.array([
+    [0, 1],
+    [-1, 0],
+    [1, -1],
+  ])  # B site at (2./3, 1./3)
+  gvecs = np.dot(gfracs, raxes)
+  rg = np.einsum('id,jd->ij', rvecs, gvecs)
+  val = vm*2*np.cos(rg+phi).sum(axis=-1)
+  return val
