568c5346bd41879222e45215dc9df15ba7692788
Yubo "Paul" Yang <pyang@flatironinstitute.org>
2023-04-07 17:33:13 -0400

    ham_one_body in PW basis

--- mch/pot/mpot.py
+++ mch/pot/mpot.py
@@ -20,3 +20,31 @@ def mpot(rvecs, am, phi, vm):
   rg = np.einsum('id,jd->ij', rvecs, gvecs)
   val = vm*2*np.cos(rg+phi).sum(axis=-1)
   return val
+
+def pw_init(raxes, ecut):
+  from qharv.inspect import axes_pos
+  from qharv.seed.hamwf_h5 import get_ksphere
+  kcut = (2*ecut)**0.5
+  kvecs = get_ksphere(raxes, kcut)
+  gvecs = axes_pos.get_nvecs(raxes, kvecs)
+  return gvecs
+
+def ham_one_body(kvecs, vkcut, vm, phi, lam=1./2):
+  # kinetic
+  kmags = np.linalg.norm(kvecs, axis=-1)
+  k2 = kmags**2
+  kmat = np.diag(lam*k2.astype(np.complex128))
+  # potential
+  kdisp = kvecs[:, np.newaxis] - kvecs[np.newaxis, :]
+  kdist = np.linalg.norm(kdisp, axis=-1)
+  sel = kdist < vkcut
+  for ik, (k1, sel1) in enumerate(zip(kvecs, sel)):
+    idx = [i for i in np.where(sel1)[0] if i != ik]
+    # connect kp, k1 separated by g
+    dkl = [kp-k1 for kp in kvecs[sel1] if not np.allclose(k1, kp)]
+    kx, ky = np.array(dkl).T
+    phis = np.arctan2(ky, kx)+np.pi/6
+    phase = np.cos(3*phis)
+    # set interaction matrix elements
+    kmat[ik, idx] = vm*np.exp(1j*phase*phi)
+  return kmat
